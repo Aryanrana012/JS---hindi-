c84942efbf22c3005d9fbcc583df3dcfc2a787c1
Aryanrana012 <ar2707477@gmail.com>
2025-08-25 13:08:18 +0530

    IMIF

---
+++ immediatelyInvokedFunction.js
@@ -0,0 +1,14 @@
+// IIFE  immediately invoked function expression
+
+(function name(){
+    console.log("aryan rana ");
+})();
+
+// () is used to ru the function immediately after its declaration 
+
+// ()   ()  first () is for function declaration and second () is to run the function
+
+( (name) => {
+    console.log("Db connected");
+    console.log(`congruatulations you are connected ${name}`);
+})('aryan rana ');
