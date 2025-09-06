2af515aaea3c4b0b5357e2f9a7f39e3c7cb0706d
Aryanrana012 <ar2707477@gmail.com>
2025-09-06 13:26:46 +0530

    reorganized folders and files

--- beginner syntax/01var.js
+++ beginner syntax/01var.js
@@ -7,7 +7,7 @@ let accountState;
 accountEmail = "aryan3162rana@gmail.com";
 accountPaassword = "rana3162@123";
 accountCity  = "shimla";
-// accountId = 234567;  we can,t assign a new value toa  constant variable;
+// accountId = 234567;  we can,t assign a new value to  a  constant variable;
 
 console.table([accountId, accountEmail, accountPassword,accountCity,accountId,accountState]);
 
